48a833a15b6939dc29fd235504e91f5651915794
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2023-06-26 12:02:31 +0300

    first commit

---
+++ cypress/e2e/testNew.cy.js
@@ -0,0 +1,22 @@
+///<reference types = "cypress"/>
+
+
+describe('My test cases', ()=>{
+    
+    beforeEach(()=>{
+        cy.clearCookies();
+    })
+    
+    
+    it('Opening a new application', ()=>{
+
+       cy.visit('/registration_form');
+
+        
+
+
+
+
+    })
+
+})
